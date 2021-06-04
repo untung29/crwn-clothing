33536af68951e1723500c21494e1f9ab1e2a4136
untung29 <uunt0001@student.monash.edu>
2021-06-04 17:14:27 +0700

    Homepage, Directory, and MenuItem components created

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../component/directory/directory.component";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
