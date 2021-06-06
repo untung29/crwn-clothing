1ad5e888d15c0d03fa1fba149c125c7486725c79
untung29 <uunt0001@student.monash.edu>
2021-06-06 16:11:11 +0700

    Adding collection-menu and collection-preview component with its styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component.jsx";
 
-const HatsPage = () => {
-  return <div>Hats Page</div>;
-};
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shops/hats" component={HatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
