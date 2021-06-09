a201ccf7c93c4dcde17adabd3e0b177c4a403241
untung29 <u.untung0029@gmail.com>
2021-06-09 12:31:00 +0700

    Sign in feature with email and password

--- src/component/sign-in/sign-in.component.jsx
+++ src/component/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import "./sign-in.styles.scss";
 
 import CustomButton from "../../component/custom-button/custom-button.component.jsx";
 import FormInput from "../../component/form-input/form-input.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils.js";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils.js";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,9 +14,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (event) => {
