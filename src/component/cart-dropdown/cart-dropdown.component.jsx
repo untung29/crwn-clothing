208f716ca7afcfafeb5cfff242d8f025abefcd59
untung29 <u.untung0029@gmail.com>
2021-06-14 18:05:11 +0700

    created cart-item component and connected our cart dropdown with cartItems

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((cartItem) => {
+          return <CartItem key={cartItem.id} item={cartItem} />;
+        })}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return { cartItems: cartItems };
+};
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/component/cart-icon/cart-icon.component.jsx
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -9,7 +9,7 @@ const CartIcon = ({ toggleCartHidden }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span class="item-count">0</span>
+      <span className="item-count">0</span>
     </div>
   );
 };
